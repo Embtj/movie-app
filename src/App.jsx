e2d0113a08c8617d4aa81ce87316e744a13249bd
Embtj <60002379+Embtj@users.noreply.github.com>
2026-05-31 21:36:32 -0500

    Fetch and display movies from TMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import MovieCard from './components/MovieCard'
 
 export default function App() {
+
+  const [movies, setMovies] = useState([])
+  
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+  function getMovie() {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setMovies(data.results)
+    }  
+    )
+  }
+
+  useEffect(() => {
+    getMovie()
+  }, [])
+
+  const movieElements = movies.map(movie => (
+    <MovieCard 
+      key={movie.id}
+      poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+      title={movie.title}
+      year={movie.release_date ? movie.release_date.split("-")[0] : "N/A"}
+    />
+  ))
+
   return (
     <div className="app-container">
       <Header />
       <section className="movie-grid">
-        <MovieCard />
-        <MovieCard />
+        {movieElements}
       </section>
     </div>
   )
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import moviePoster from "../assets/movie-poster.jpg"
 
-export default function MovieCard() {
+export default function MovieCard(props) {
   return (
     <article className="movie-card">
-      <img src={moviePoster} alt="movie poster" className="movie-poster" />  
-      <h2 className="movie-title">Title</h2>
-      <p className="movie-year">2022</p>
+      <img src={props.poster} alt="movie poster" className="movie-poster" />  
+      <h2 className="movie-title">{props.title}</h2>
+      <p className="movie-year">{props.year}</p>
     </article>
   )
 }
