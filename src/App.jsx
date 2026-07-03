3a2bcaafac30f97a27400fe95b4537b43be8960c
Embtj <60002379+Embtj@users.noreply.github.com>
2026-07-03 18:31:52 -0500

    Set up React Router with layout route and four main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,21 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import MovieCard from './components/MovieCard'
+import { BrowserRouter, Routes, Route } from "react-router"
+import Home from "./pages/Home"
+import Layout from './components/Layout'
+import Search from "./pages/Search"
+import Watchlist from "./pages/Watchlist"
+import MovieDetails from "./pages/MovieDetails"
 
 export default function App() {
-
-  const [movies, setMovies] = useState([])
-  
-  const API_KEY = import.meta.env.VITE_TMDB_API_KEY
-
-  function getMovie() {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      setMovies(data.results)
-    }  
-    )
-  }
-
-  useEffect(() => {
-    getMovie()
-  }, [])
-
-  const movieElements = movies.map(movie => (
-    <MovieCard 
-      key={movie.id}
-      poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-      title={movie.title}
-      year={movie.release_date ? movie.release_date.split("-")[0] : "N/A"}
-    />
-  ))
-
   return (
-    <div className="app-container">
-      <Header />
-      <section className="movie-grid">
-        {movieElements}
-      </section>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movie/:id" element={<MovieDetails />} />
+          <Route path="search" element={<Search />} />
+          <Route path="watchlist" element={<Watchlist />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import MovieCard from "../components/MovieCard"
+
+function Home() {
+  const [movies, setMovies] = useState([])
+
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+  function getMovie() {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setMovies(data.results)
+      }
+      )
+  }
+
+  useEffect(() => {
+    getMovie()
+  }, [])
+
+  const movieElements = movies.map(movie => (
+    <MovieCard
+      key={movie.id}
+      poster={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+      title={movie.title}
+      year={movie.release_date ? movie.release_date.split("-")[0] : "N/A"}
+    />
+  ))
+
+  return (
+    <section className="movie-grid">
+      {movieElements}
+    </section>
+  )
+}
+
+export default Home
