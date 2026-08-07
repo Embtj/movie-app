ccad27b531a919aae685775dc8f6811d2a1cbf74
Embtj <60002379+Embtj@users.noreply.github.com>
2026-08-06 20:23:12 -0500

    Add TMDB attribution to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import tmdbLogo from "../assets/tmdb-logo.svg"
 
 function Footer() {
   return (
     <div className="footer">
-      
+      <img src={tmdbLogo} alt="TMDB logo" className="tmdb-logo" />
+      <p className="tmdb-attribution">This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.</p>
+      <p className="copyright-text">© 2026 Embla Johansen</p>
     </div>
   )
 }
