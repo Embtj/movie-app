5092b236024e47ad3040e34f35f92f598b7d6171
Embtj <60002379+Embtj@users.noreply.github.com>
2026-08-07 22:48:44 -0500

    Add empty watchlist state

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -1,15 +1,28 @@
 import { useContext } from 'react'
+import { Link } from "react-router"
 import MovieGrid from "../components/MovieGrid"
 import { WatchlistContext } from '../components/WatchlistProvider'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFilm } from "@fortawesome/free-solid-svg-icons"
 
 export default function Watchlist() {
 
   const { watchlist, removeFromWatchlist } = useContext(WatchlistContext)
 
+
   return (
     <div>
-      <h1>My Watchlist</h1>
-      <MovieGrid movies={watchlist} />
+      <h1 className="watchlist-heading">My Watchlist</h1>
+      {watchlist.length === 0 ? (
+        <div className="watchlist-empty">
+          <FontAwesomeIcon icon={faFilm} className="film-icon" />
+          <h2 className="watchlist-empty-heading">Your watchlist is empty</h2>
+          <p className="watchlist-empty-text">Start building your watchlist by adding movies you want to watch</p>
+          <Link to="/" className="browse-movies-link">Browse movies</Link>
+        </div>
+      ) : (
+        <MovieGrid movies={watchlist} />
+      )}
     </div>
   )
 }
